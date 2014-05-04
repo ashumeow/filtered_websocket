0500612471bf0b59e6d9047275131942d7f85ab7
Morgan Phillips <morgan@artistgrowth.com>
2014-05-04 11:55:40 -0500

    Fixes broken setup.py

--- setup.py
+++ setup.py
@@ -25,7 +25,16 @@ setup(
         "filtered_websocket.filters",
         "filtered_websocket.storage_objects",
     ],
-    long_description=read("README.md"),
+    long_description="""
+    Filtered WebSocket is a straight forward framework for implementing websocket servers which draws inspiration from UNIX process pipelines.
+    It's a metaphor for: cat server_event | behavior_a | behavior_b | ...
+    Server event handlers are encapsulated within filters such that building elaborate behaviors is as easy as importing new modules.
+    Imported filters automatically add themselves to an appropriate filter chain (pipeline).
+
+    - Supports SSL
+    - Scales horizontally via remote backend storage (redis)
+    - Supports token based auth
+    - New behaviors are added by simply importing filter modules""",
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Topic :: Utilities",
