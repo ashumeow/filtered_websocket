6bc93886eca00b51967d56ed03de6521d66de71c
Morgan Phillips <morgan@artistgrowth.com>
2014-07-08 00:58:08 -0500

    Removes un-needed examples folder.

--- examples/broadcast_server/start_server.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-
-####
-# broadcasts messages to all connected clients and prints all data which 
-# passes through it to stdout.
-####
-
-python -m filtered_websocket.server -f "filters.broadcast_messages" "filters.stdout_rawdata"
